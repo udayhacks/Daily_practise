9a1a72c861406ff319798ce8692c3d3d72c076ec
uday kanchanapally <71202015+udayhacks@users.noreply.github.com>
2026-04-07 11:34:15 +0530

    Create 3246-CheckIfBitwiseOrHasTrailingZeros

---
+++ 3246-CheckIfBitwiseOrHasTrailingZeros/3246-CheckIfBitwiseOrHasTrailingZeros.py
@@ -0,0 +1,10 @@
+# Last updated: 07/04/2026, 11:34:15
+class Solution:
+    def hasTrailingZeros(self, nums: List[int]) -> bool:
+        for i in range(len(nums)) :
+            for j in range(i+1 , len(nums)):
+                k = bin((nums[i]|nums[j]))
+                if k[-1] == '0' :
+                        return True 
+        return False
+        
