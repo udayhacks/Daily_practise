9c68eb6258931069fefa99a49f25b5be51f7435e
uday kanchanapally <71202015+udayhacks@users.noreply.github.com>
2026-04-07 11:34:42 +0530

    Create 2809-CreateHelloWorldFunction

---
+++ 2809-CreateHelloWorldFunction/2809-CreateHelloWorldFunction.js
@@ -0,0 +1,15 @@
+// Last updated: 07/04/2026, 11:34:42
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+                                                        return "Hello World"
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
