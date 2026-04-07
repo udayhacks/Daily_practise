49c4910a81da153935094950fbec62b1f6b5840c
uday kanchanapally <71202015+udayhacks@users.noreply.github.com>
2026-04-07 11:33:44 +0530

    Create 3779-EatPizzas

---
+++ 3779-EatPizzas/3779-EatPizzas.py
@@ -0,0 +1,22 @@
+# Last updated: 07/04/2026, 11:33:44
+class Solution:
+   
+    def maxWeight(self, pizzas) -> int:
+        n = len(pizzas)
+        days = n//4 
+        odd_days = days//2
+        if( days%2 !=0 ) :odd_days = 1+days//2
+    
+        pizzas.sort()
+        ans = 0 
+        
+        for i in range(days):
+            if odd_days:
+                ans+=pizzas[n-1]
+                n = n-1
+                odd_days-=1
+            else:
+                ans+=pizzas[n-2]
+                n = n-2
+
+        return ans
