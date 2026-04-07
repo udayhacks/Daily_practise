d1597b7aa6a1e363f1a0b0d40e9b555d8b467196
uday kanchanapally <71202015+udayhacks@users.noreply.github.com>
2026-04-07 11:37:56 +0530

    Create 782-JewelsAndStones

---
+++ 782-JewelsAndStones/782-JewelsAndStones.py
@@ -0,0 +1,12 @@
+# Last updated: 07/04/2026, 11:37:56
+
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+
+        jewel_map = {j:0 for j in jewels}
+
+        for s in stones:
+            if s in jewel_map:
+                jewel_map[s] += 1
+        
+        return sum(jewel_map.values())
