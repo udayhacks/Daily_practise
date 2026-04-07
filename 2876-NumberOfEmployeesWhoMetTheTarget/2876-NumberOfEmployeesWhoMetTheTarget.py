82532e93228524d158038ed626bb8451affe79ac
uday kanchanapally <71202015+udayhacks@users.noreply.github.com>
2026-04-07 11:34:36 +0530

    Create 2876-NumberOfEmployeesWhoMetTheTarget

---
+++ 2876-NumberOfEmployeesWhoMetTheTarget/2876-NumberOfEmployeesWhoMetTheTarget.py
@@ -0,0 +1,10 @@
+# Last updated: 07/04/2026, 11:34:36
+class Solution:
+    def numberOfEmployeesWhoMetTarget(self, hours: List[int], target: int) -> int:
+        count = 0 
+        for i in range(len(hours)):
+            if hours[i] >=target :
+                count +=1
+        return count 
+                
+        
