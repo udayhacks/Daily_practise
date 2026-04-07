e0f5b4955f2e9ef2663b85f6f71ac216effa527c
uday kanchanapally <71202015+udayhacks@users.noreply.github.com>
2026-04-07 11:37:35 +0530

    Create 943-SumOfSubarrayMinimums

---
+++ 943-SumOfSubarrayMinimums/943-SumOfSubarrayMinimums.py
@@ -0,0 +1,22 @@
+# Last updated: 07/04/2026, 11:37:34
+
+class Solution:
+    def sumSubarrayMins(self, arr: List[int]) -> int:
+        stack = []
+        result = 0
+        mod = 10**9 + 7
+
+        for i, num in enumerate(arr):
+            while stack and num < arr[stack[-1]]:
+                idx = stack.pop()
+                result += arr[idx] * (i - idx) * (idx - (stack[-1] if stack else -1))
+                result %= mod
+
+            stack.append(i)
+
+        while stack:
+            idx = stack.pop()
+            result += arr[idx] * (len(arr) - idx) * (idx - (stack[-1] if stack else -1))
+            result %= mod
+
+        return result
