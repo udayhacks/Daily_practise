fbd0493465a3000cb4d376b73d5eac7e9858b069
uday kanchanapally <71202015+udayhacks@users.noreply.github.com>
2026-04-07 11:41:57 +0530

    Create 75-SortColors

---
+++ 75-SortColors/75-SortColors.py
@@ -0,0 +1,28 @@
+# Last updated: 07/04/2026, 11:41:57
+class Solution:
+    def sortColors(self, nums) :
+
+        
+        def sortArray(arr):
+                low = 0
+                mid = 0
+                high = len(arr) - 1
+
+                while mid <= high:
+                    if arr[mid] == 0:
+                        arr[low], arr[mid] = arr[mid], arr[low]
+                        low += 1
+                        mid += 1
+                    elif arr[mid] == 1:
+                        mid += 1
+                    else:
+                        arr[mid], arr[high] = arr[high], arr[mid]
+                        high -= 1
+
+        sortArray(nums)
+
+        
+        
+        return nums
+
+
