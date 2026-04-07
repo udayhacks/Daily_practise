1a2891d4958381cf2394c1c123d839796c54e273
uday kanchanapally <71202015+udayhacks@users.noreply.github.com>
2026-04-07 11:39:13 +0530

    Create 380-InsertDeleteGetrandomO1

---
+++ 380-InsertDeleteGetrandomO1/380-InsertDeleteGetrandomO1.py
@@ -0,0 +1,35 @@
+# Last updated: 07/04/2026, 11:39:12
+class RandomizedSet:
+
+    def __init__(self):
+
+        self.set = []
+        
+
+    def insert(self, val: int) -> bool:
+        if val not in self.set :
+            self.set.append(val)
+            return True
+        return False
+        
+
+    def remove(self, val: int) -> bool:
+        if val in self.set :
+            self.set.remove(val)
+            return True 
+        return False
+        
+
+    def getRandom(self) -> int:
+        return random.choice(self.set)
+
+
+
+        
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
