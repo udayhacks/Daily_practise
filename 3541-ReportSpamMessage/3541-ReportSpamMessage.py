5882980e5201e08cab9997501b93ec360d6f3847
uday kanchanapally <71202015+udayhacks@users.noreply.github.com>
2026-04-07 11:33:58 +0530

    Create 3541-ReportSpamMessage

---
+++ 3541-ReportSpamMessage/3541-ReportSpamMessage.py
@@ -0,0 +1,17 @@
+# Last updated: 07/04/2026, 11:33:58
+class Solution:
+    def reportSpam(self, message: List[str], bannedWords: List[str]) -> bool:
+        banned = set(bannedWords)
+        count = 0
+
+        for msg in message:
+            if msg in banned:
+                count += 1
+                if count == 2:
+                    return True
+        return False
+                
+            
+
+            
+        
