d9ca8bbab3efe88fcf8aa10cde3e93418527c56e
uday kanchanapally <71202015+udayhacks@users.noreply.github.com>
2026-04-07 11:35:05 +0530

    Create 2413-SmallestNumberInInfiniteSet

---
+++ 2413-SmallestNumberInInfiniteSet/2413-SmallestNumberInInfiniteSet.py
@@ -0,0 +1,21 @@
+# Last updated: 07/04/2026, 11:35:05
+class SmallestInfiniteSet:
+
+    def __init__(self):
+        self.arr =  [i for i in range(1,1001)]
+
+    def popSmallest(self) -> int:
+        return heapq.heappop(self.arr)
+
+        
+
+    def addBack(self, num: int) -> None:
+        if num not in self.arr:
+            heapq.heappush(self.arr,num)
+        
+
+
+# Your SmallestInfiniteSet object will be instantiated and called as such:
+# obj = SmallestInfiniteSet()
+# param_1 = obj.popSmallest()
+# obj.addBack(num)
