0725cb0b1abc807361567d4413deaf57ed38e849
uday kanchanapally <71202015+udayhacks@users.noreply.github.com>
2026-04-07 11:35:29 +0530

    Create 2048-BuildArrayFromPermutation

---
+++ 2048-BuildArrayFromPermutation/2048-BuildArrayFromPermutation.py
@@ -0,0 +1,8 @@
+# Last updated: 07/04/2026, 11:35:28
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        l = []
+
+        for i in range(len(nums)):
+            l.append(nums[nums[i]])
+        return l
