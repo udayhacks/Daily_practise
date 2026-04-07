a1c8b02e701a0067dc67da165a68189e6b21b9b3
uday kanchanapally <71202015+udayhacks@users.noreply.github.com>
2026-04-07 11:37:18 +0530

    Create 1034-SubarraysWithKDifferentIntegers

---
+++ 1034-SubarraysWithKDifferentIntegers/1034-SubarraysWithKDifferentIntegers.py
@@ -0,0 +1,21 @@
+# Last updated: 07/04/2026, 11:37:18
+class Solution:
+    def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
+        return self.subarraysWithAtMostKDistinct(nums, k) - self.subarraysWithAtMostKDistinct(nums, k - 1)
+
+    def subarraysWithAtMostKDistinct(self, nums, k):
+        ans = 0
+        count = [0] * (len(nums) + 1)
+
+        l = 0
+        for r in range(len(nums)):
+            count[nums[r]] += 1
+            if count[nums[r]] == 1:
+                k -= 1
+            while k == -1:
+                count[nums[l]] -= 1
+                if count[nums[l]] == 0:
+                    k += 1
+                l += 1
+            ans += r - l + 1
+        return ans
