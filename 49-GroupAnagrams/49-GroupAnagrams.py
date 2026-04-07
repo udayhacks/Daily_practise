641daae9684d3f0be39b10275ebd29124582c536
uday kanchanapally <71202015+udayhacks@users.noreply.github.com>
2026-04-07 11:42:25 +0530

    Create 49-GroupAnagrams

---
+++ 49-GroupAnagrams/49-GroupAnagrams.py
@@ -0,0 +1,10 @@
+# Last updated: 07/04/2026, 11:42:25
+class Solution:
+    def groupAnagrams(self, strs):
+        anagram_map = defaultdict(list)
+        
+        for word in strs:
+            sorted_word = ''.join(sorted(word))
+            anagram_map[sorted_word].append(word)
+        
+        return list(anagram_map.values())
