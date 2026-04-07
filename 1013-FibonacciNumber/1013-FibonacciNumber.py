fb1be768027f4477a2f791848f7a72aa17fdbac2
uday kanchanapally <71202015+udayhacks@users.noreply.github.com>
2026-04-07 11:37:26 +0530

    Create 1013-FibonacciNumber

---
+++ 1013-FibonacciNumber/1013-FibonacciNumber.py
@@ -0,0 +1,16 @@
+# Last updated: 07/04/2026, 11:37:25
+class Solution:
+    def fib(self, n: int) -> int:
+        def fib(n):
+            if Fib[n] != -1:
+                return Fib[n]
+            Fib[n] = fib(n-1) + fib(n-2)
+            return Fib[n]
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        Fib = [-1 for _ in range(n+1)]
+        Fib[0] = 0
+        Fib[1] = 1
+        return fib(n)
