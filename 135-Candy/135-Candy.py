6aa67c1d3e67de9659491fba411b871ee3386898
uday kanchanapally <71202015+udayhacks@users.noreply.github.com>
2026-04-07 11:41:05 +0530

    Create 135-Candy

---
+++ 135-Candy/135-Candy.py
@@ -0,0 +1,16 @@
+# Last updated: 07/04/2026, 11:41:05
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        n = len(ratings)
+        dp = [1]*n
+        #left to right
+        for i in range(1,n):
+            if ratings[i]>ratings[i-1] and dp[i]<=dp[i-1]:
+                dp[i] = dp[i-1]+1
+        #right to left
+        for i in range(n-2,-1,-1):
+            if ratings[i]>ratings[i+1] and dp[i]<=dp[i+1]:
+                dp[i] = dp[i+1]+1
+        return sum(dp)
+
+        
