4551aae4390634564b3f07205914f86af4f4dd2e
uday kanchanapally <71202015+udayhacks@users.noreply.github.com>
2026-04-07 11:41:52 +0530

    Create 80-RemoveDuplicatesFromSortedArrayIi

---
+++ 80-RemoveDuplicatesFromSortedArrayIi/80-RemoveDuplicatesFromSortedArrayIi.cpp
@@ -0,0 +1,18 @@
+// Last updated: 07/04/2026, 11:41:51
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int k = 2;
+
+        if (nums.size() <= 2) return nums.size();
+        
+        for (int i = 2; i < nums.size(); i++) {
+            if (nums[i] != nums[k - 2]) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;       
+    }
+};
