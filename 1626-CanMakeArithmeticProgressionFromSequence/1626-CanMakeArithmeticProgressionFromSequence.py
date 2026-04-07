1493fded9136539590edbfa07165d9e425f0b8ad
uday kanchanapally <71202015+udayhacks@users.noreply.github.com>
2026-04-07 11:36:08 +0530

    Create 1626-CanMakeArithmeticProgressionFromSequence

---
+++ 1626-CanMakeArithmeticProgressionFromSequence/1626-CanMakeArithmeticProgressionFromSequence.py
@@ -0,0 +1,11 @@
+# Last updated: 07/04/2026, 11:36:08
+class Solution:
+    def canMakeArithmeticProgression(self, arr: List[int]) -> bool:
+        arr.sort()
+        d = arr[1]-arr[0]
+        for i in range(2,len(arr)):
+            if arr[i]-arr[i-1]!= d:
+                return False
+        return True 
+
+            
