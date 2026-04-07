9cf28afffe74e482f0250a511c7dc8ee7e6df6a8
uday kanchanapally <71202015+udayhacks@users.noreply.github.com>
2026-04-07 11:42:07 +0530

    Create 66-PlusOne

---
+++ 66-PlusOne/66-PlusOne.py
@@ -0,0 +1,15 @@
+# Last updated: 07/04/2026, 11:42:07
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        for i in range(len(digits)-1,-1,-1):
+            if digits[i]==9 :
+                digits[i] =0 
+            else:
+                digits[i]+=1
+                return digits
+
+        return [1]+digits 
+                
+            
+             
+
