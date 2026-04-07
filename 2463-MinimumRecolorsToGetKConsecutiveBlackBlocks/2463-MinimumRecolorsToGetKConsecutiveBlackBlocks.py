93b127c87d20407933d15a590bccb335defd6ff5
uday kanchanapally <71202015+udayhacks@users.noreply.github.com>
2026-04-07 11:35:03 +0530

    Create 2463-MinimumRecolorsToGetKConsecutiveBlackBlocks

---
+++ 2463-MinimumRecolorsToGetKConsecutiveBlackBlocks/2463-MinimumRecolorsToGetKConsecutiveBlackBlocks.py
@@ -0,0 +1,22 @@
+# Last updated: 07/04/2026, 11:35:02
+class Solution:
+    def minimumRecolors(self, blocks: str, k: int) -> int:
+        #declartion 
+        white = 0 
+        recolor = float('inf') 
+        n = len(blocks)
+        l = 0 
+        for r in range(n):
+            if blocks[r] == 'W':
+                    white+=1
+
+            if r-l+1 >k :
+                    if blocks[l] == 'W':
+                        white-=1
+                    l+=1
+            
+            if r-l+1 == k :
+                    recolor = min(recolor,white)
+                
+                
+        return recolor
