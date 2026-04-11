f5df9166d377f86db34d8ba74ff31fcbc442afb0
uday kanchanapally <71202015+udayhacks@users.noreply.github.com>
2026-04-11 17:25:36 +0530

    File updated at 11/04/2026, 17:25:35

--- 3741-MinimumDistanceBetweenThreeEqualElementsII/3741-MinimumDistanceBetweenThreeEqualElementsII.java
+++ 3741-MinimumDistanceBetweenThreeEqualElementsII/3741-MinimumDistanceBetweenThreeEqualElementsII.java
@@ -1,50 +1,62 @@
-// Last updated: 11/04/2026, 17:10:37
+// Last updated: 11/04/2026, 17:25:35
 1import java.util.*;
 2
 3class Solution {
-4    public int minimumDistance(int[] nums) {
-5
-6        HashMap<Integer, ArrayList<Integer>> mp = new HashMap<>();
-7
-8        int n = nums.length;
-9
-10        // Build map
-11        for (int i = 0; i < n; i++) {
+4
+5    private HashMap<Integer,ArrayList<Integer>> map = new HashMap<>();
+6
+7    private HashMap<Integer,ArrayList<Integer>> build(int[] nums){
+8        
+9        
+10        
+11        int n = nums.length;
 12
-13            if (mp.containsKey(nums[i])) {
-14                mp.get(nums[i]).add(i);
-15
-16            } else {
-17                ArrayList<Integer> list = new ArrayList<>();
-18                list.add(i);
-19                mp.put(nums[i], list);
-20            }
-21        }
-22
-23        int maxVal = Integer.MAX_VALUE;
+13        for ( int i = 0;i<n;i++){
+14
+15            if ( map.containsKey(nums[i])){
+16                map.get(nums[i]).add(i);
+17            }else{
+18                ArrayList <Integer> values = new ArrayList<>();
+19
+20                values.add(i);
+21                map.put(nums[i],values);
+22            }
+23        }
 24
-25        // Iterate map
-26        for (Map.Entry<Integer, ArrayList<Integer>> map : mp.entrySet()) {
+25        return map;
+26
 27
-28            ArrayList<Integer> list = map.getValue();
+28    }
 29
-30            if (list.size() >= 3) {
+30    private int cal(){
 31
-32                int m = list.size();
+32        int minVal = Integer.MAX_VALUE; 
 33
-34                for (int i = 0; i < m - 2; i++) {
-35
-36                    int v1 = list.get(i);
-37                    int v3 = list.get(i + 2);
+34        
+35        
+36    
+37    for ( Map.Entry<Integer, ArrayList<Integer>> entry : map.entrySet()){
 38
-39                    // Correct formula
-40                    int val = 2 * (v3 - v1);
-41
-42                    maxVal = Math.min(val, maxVal);
-43                }
-44            }
-45        }
-46
-47        return (maxVal == Integer.MAX_VALUE) ? -1 : maxVal;
-48    }
-49}
+39        ArrayList <Integer> list = entry.getValue();
+40
+41        if ( list.size()>=3){
+42
+43            for ( int i = 0;i<list.size()-2;i++){
+44                int val = 2*(list.get(i+2)-list.get(i));
+45                minVal = Math.min(minVal, val);
+46            }
+47        }
+48        
+49
+50    } return (minVal == Integer.MAX_VALUE) ? -1 : minVal;
+51
+52
+53    }
+54    
+55
+56
+57    public int minimumDistance(int[] nums) {
+58            build(nums);
+59            return cal();
+60    }
+61}
