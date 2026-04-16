7c7d6ad7e69691afa5a734d82918d357d45d75a9
uday kanchanapally <71202015+udayhacks@users.noreply.github.com>
2026-04-16 11:32:48 +0530

    File updated at 16/04/2026, 11:32:47

--- 56-MergeIntervals/56-MergeIntervals.java
+++ 56-MergeIntervals/56-MergeIntervals.java
@@ -1,4 +1,4 @@
-// Last updated: 16/04/2026, 11:31:44
+// Last updated: 16/04/2026, 11:32:47
 1class Solution {
 2    public int[][] merge(int[][] intervals) {
 3        
@@ -6,32 +6,34 @@
 5        // sorting according to starting value ASCE 
 6
 7        Arrays.sort( intervals , (a,b)-> a[0]-b[0]);
-8
-9
-10        int n = intervals.length;
-11        List<int[]> result = new ArrayList<>();
-12
-13        int[] current = intervals[0];
+8        
+9        int [][] freak = new int[3][4];
+10
+11
+12        int n = intervals.length;
+13        List<int[]> result = new ArrayList<>();
 14
-15        for ( int i =1 ; i<n;i++){
-16            int [] next = intervals[i];
-17
-18            if ( current[1] >= next[0]){
-19                current[1] = Math.max(current[1],next[1]);
-20
-21            }else{
-22                result.add(current);
-23                current = next;
-24            }
-25
-26        }
-27        result.add(current);
-28
-29        return result.toArray(new int[result.size()][2]);
+15        int[] current = intervals[0];
+16
+17        for ( int i =1 ; i<n;i++){
+18            int [] next = intervals[i];
+19
+20            if ( current[1] >= next[0]){
+21                current[1] = Math.max(current[1],next[1]);
+22
+23            }else{
+24                result.add(current);
+25                current = next;
+26            }
+27
+28        }
+29        result.add(current);
 30
-31
+31        return result.toArray(new int[result.size()][2]);
 32
 33
 34
-35    }
-36}
+35
+36
+37    }
+38}
