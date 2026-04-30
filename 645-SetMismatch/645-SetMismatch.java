0c831838dab74a9392714e1f5947cbb1c840d58c
uday kanchanapally <71202015+udayhacks@users.noreply.github.com>
2026-04-30 16:51:16 +0530

    File updated at 30/04/2026, 16:51:16

--- 645-SetMismatch/645-SetMismatch.java
+++ 645-SetMismatch/645-SetMismatch.java
@@ -1,31 +1,28 @@
-// Last updated: 30/04/2026, 16:23:46
+// Last updated: 30/04/2026, 16:51:16
 1class Solution {
-2    public int[] findErrorNums(int[] nums) {
-3
+2    public int[] smallerNumbersThanCurrent(int[] nums) {
+3        
 4        int n = nums.length;
-5        int[] a = new int[n + 1];
+5        int [] temp = nums.clone();
 6
-7        // initialize with -1
-8        for (int i = 0; i <= n; i++) {
-9            a[i] = -1;
-10        }
-11
-12        int r = -1;
-13
-14        for (int i = 0; i < n; i++) {
-15            if (a[nums[i]] != -1) r = nums[i];
-16            if (a[nums[i]] == -1) a[nums[i]] = i;
-17        }
+7        Arrays.sort(temp);
+8        
+9        Map<Integer,Integer> map = new HashMap<>();
+10
+11        for ( int i = 0 ; i<n;i++){
+12            if ( !map.containsKey(temp[i])){
+13                map.put(temp[i],i);
+14            }
+15        }
+16
+17        int[] res = new int[n];
 18
-19        int m = -1;
-20
-21        for (int i = 1; i <= n; i++) {
-22            if (a[i] == -1) {
-23                m = i;
-24                break;
-25            }
-26        }
-27
-28        return new int[]{r, m};
-29    }
-30}
+19        for ( int i = 0 ;i <n;i++){
+20            res[i] = map.get(nums[i]);
+21        }
+22        return res;
+23
+24
+25        
+26    }
+27}
