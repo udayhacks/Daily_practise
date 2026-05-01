59788970605d13ca19e9759c542f1efcc0b8f472
uday kanchanapally <71202015+udayhacks@users.noreply.github.com>
2026-05-01 08:21:57 +0530

    File updated at 01/05/2026, 08:21:57

--- 1441-BuildanArrayWithStackOperations/1441-BuildanArrayWithStackOperations.java
+++ 1441-BuildanArrayWithStackOperations/1441-BuildanArrayWithStackOperations.java
@@ -1,27 +1,29 @@
-// Last updated: 01/05/2026, 07:58:31
-1class Solution {
-2    public List<String> buildArray(int[] target, int n) {
-3
-4        int N = target.length;
-5
-6       
-7        int [] temp = new int[target[N-1]+1];
-8
-9        for ( int  i : target){
-10            temp[i] = i;
-11        }
-12
-13        List<String> result = new ArrayList<>();
-14
-15        for ( int i = 1; i<temp.length;i++){
-16            result.add("Push");
-17            if ( temp[i] != i ){
-18                result.add("Pop");
-19            }
-20        }
-21        return result;
-22
-23
-24
-25    }
-26}
+// Last updated: 01/05/2026, 08:21:57
+class Solution {
+    public List<String> buildArray(int[] target, int n) {
+        int last=target[target.length-1];
+int j=0;
+        List<String> answer=new ArrayList<>();
+List<Integer> list = Arrays.stream(target)
+                           .boxed() 
+                           .toList(); 
+      for(int i=1;i<=last;i++){
+        answer.add("Push");
+        if(target[j]==i){
+            j++;
+        }
+        else{
+    answer.add("Pop");
+    }
+}
+
+//         if(list.contains(i)!=true){
+// answer.add("Pop");
+//         }
+
+                           
+
+
+        return answer;
+    }
+}
